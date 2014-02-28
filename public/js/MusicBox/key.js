ded684b004fe8989b378bbaf3b05f55b6c310e73
tomplays <homeof@gmail.com>
2014-02-28 17:13:13 +0100

    angular controllers for fragment

---
+++ public/js/MusicBox/key.js
@@ -0,0 +1,6 @@
+'use strict';
+//console.log('KeyCtrl      @ /public/js/MusicBox/key.js');
+function KeyCtrl($scope,$http,  docsfactory) {
+	// not included anymore , added to DocumentCtrl/services 
+}
+//KeyCtrl.$inject = ['$scope', '$http', 'docsfactory'];
