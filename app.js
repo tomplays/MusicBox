a4f1080f8a2b6d44df6c131efbada5a2b779f088
tomplays <homeof@gmail.com>
2014-09-22 01:44:17 +0200

    new files

--- app.js
+++ app.js
@@ -20,10 +20,15 @@ var package = require('./package.json'),
     express = require('express'),
     http = require('http'),
     path = require('path'),
+    cors = require('cors'),
     fs = require('fs');
+
   var program = require('commander');
     var request = require('request');
 
+var passport = require('passport')
+, flash = require('connect-flash')
+, LocalStrategy = require('passport-local').Strategy;
 
 nconf.argv().env().file({file:'config.json'});
 program
@@ -33,6 +38,131 @@ program
   
 var app = express();
 // views and misc
+
+ 
+
+
+//
+var users = new Array()
+var r = request.get('http://localhost:3001/apis/profiles/list/'+nconf.get('API_SECRET'), function optionalCallback (err, httpResponse, body) {
+  if (err) {
+    return console.error('upload failed:', err);
+  }
+  users  = JSON.parse(body)
+  console.log(users)
+  //app.set('users',users);
+  return users;
+  
+})
+
+
+function findById(id, fn) {
+    console.log('useazeazr ok')
+var idx = id - 1;
+if (users[idx]) {
+fn(null, users[idx]);
+} else {
+fn(new Error('User ' + id + ' does not exist'));
+}
+}
+function findByUsername(username, fn) {
+    console.log('user zezaeok')
+for (var i = 0, len = users.length; i < len; i++) {
+var user = users[i];
+if (user.username === username) {
+return fn(null, user);
+}
+}
+return fn(null, null);
+}
+
+function findById___(id, fn) {
+
+
+
+
+
+var re = request.get('http://localhost:3001/apis/profiles/auth/2/tom', function optionalCallback (err, httpResponse, body) {
+  if (err) {
+      console.log('user erer')
+    //return console.error('upload failed:', err);
+     return fn(null, null);
+  }
+  user  = JSON.parse(body)
+   console.log('userrrrrr ok')
+  //app.set('users',users);
+  fn(null, user);
+  //return user;
+  
+})
+}
+function findByUsername__(username, fn) {
+  var re = request.get('http://localhost:3001/apis/profiles/auth/2/tom', function optionalCallback (err, httpResponse, body) {
+  if (err) {
+    //return console.error('upload failed:', err);
+          console.log('user erer')
+
+     return fn(null, null);
+  }
+  user  = JSON.parse(body)
+  console.log('useeer ok')
+  //app.set('users',users);
+  fn(null, user);
+  //return user;
+  
+})
+
+}
+
+
+
+// Passport session setup.
+// To support persistent login sessions, Passport needs to be able to
+// serialize users into and deserialize users out of the session. Typically,
+// this will be as simple as storing the user ID when serializing, and finding
+// the user by ID when deserializing.
+passport.serializeUser(function(user, done) {
+done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+findById(id, function (err, user) {
+done(err, user);
+});
+});
+// Use the LocalStrategy within Passport.
+// Strategies in passport require a `verify` function, which accept
+// credentials (in this case, a username and password), and invoke a callback
+// with a user object. In the real world, this would query a database;
+// however, in this example we are using a baked-in set of users.
+passport.use(new LocalStrategy(
+function(username, password, done) {
+// asynchronous verification, for effect...
+process.nextTick(function () {
+// Find the user by username. If there is no user with the given
+// username, or the password is not correct, set the user to `false` to
+// indicate failure and set a flash message. Otherwise, return the
+// authenticated `user`.
+findByUsername(username, function(err, user) {
+if (err) { return done(err); }
+if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+return done(null, user);
+})
+});
+}
+));
+
+
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+    res.redirect('/login')
+}
+
+
+
+var app = express();
+// views and misc
 app.configure(function(){
   app.set('port',nconf.get('PORT') );
   app.set('views', __dirname + '/views');
@@ -40,13 +170,32 @@ app.configure(function(){
   app.set('view options', {
     layout: true
   });
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
+ // app.use(express.bodyParser());
+ // app.use(express.methodOverride());
+//  app.use(cors())
+  //app.use(express.cookieParser(  nconf.get('COOKIESECRET') ) );
+  //app.use(express.session( nconf.get('SESSIONSECRET') ) );
+ 
 
-  app.use(express.cookieParser(  nconf.get('COOKIESECRET') ) );
-  app.use(express.session( nconf.get('SESSIONSECRET') ) );
+
+ //app.use(express.logger());
+//app.use(flash());
+// Initialize Passport! Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+//app.use(passport.initialize());
+//app.use(passport.session());
+
+
+
+
   app.use(express.static(__dirname + '/public'));
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.use(app.router);
+
 });
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: false , showStack: false }));
@@ -56,6 +205,12 @@ app.configure('production', function(){
 });
 // Routes
 var routes = require('./routes')(app);
+
+
+
+
+
+
 
 // Start server
 var server =  http.createServer(app).listen(app.get('port'), function(){
