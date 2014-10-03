144d5f922aa897d2c3ff705277b55753ce73ee97
System Administrator <root@gerald.local>
2014-10-03 12:53:56 +0200

    initial version

---
+++ less.sh
@@ -0,0 +1,3 @@
+lessc /Applications/MAMP/htdocs/sites/hacktuel-final/MusicBoxMongo/MusicBox/public/css/grid.less > /Applications/MAMP/htdocs/sites/hacktuel-final/MusicBoxMongo/MusicBox/public/css/grid.css
+lessc /Applications/MAMP/htdocs/sites/hacktuel-final/MusicBoxMongo/MusicBox/public/css/charset.less > /Applications/MAMP/htdocs/sites/hacktuel-final/MusicBoxMongo/MusicBox/public/css/charset.css
+
