d6ae806335f88f19344cc90e1fcb213929314677
tomplays <homeof@gmail.com>
2014-10-03 16:20:40 +0200

    new css minification / grunt watch

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+     pkg: grunt.file.readJSON('package.json'),
+    less: {
+       options: {
+            paths: ["public/css"],
+            compress: true
+        },
+
+        src: {
+            // no need for files, the config below should work
+            expand: true,
+            cwd:    "public/css",
+            src:    "**/*.less",
+            dest:   "public/css/min",
+            ext:    ".css",   
+        },     
+    },
+    jasmine: {
+        test: {
+
+            src: 'public/js/MusicBox/document_v1.js',
+             options: {
+                specs: 'tests/spec.js',
+                display: 'short',
+                summary: true,
+                keepRunner: true
+            }
+        }
+
+    },
+    connect: {
+      server: {
+        options: {
+          hostname: '',
+          port: 80
+        }
+      }
+    },
+    jade: {
+      compile: {
+        options: {
+          data: {
+            debug: false,
+            root_url: 'http://localhost',
+            lang_js_url : '/hello.js',
+            env: 'prod'
+          }
+        },
+       
+
+       files: {
+            // no need for files, the config below should work
+          
+           "tests/partials/*.html" : "views/partials/*.jade",
+           
+          
+        }  
+      }
+    },
+    watch: {
+      styles: {
+        files: ['public/css/*.less'], // which files to watch
+        tasks: ['less']
+      },
+      templates: {
+        files: ['views/partials/*.jade'], // which files to watch
+        tasks: ['jade']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+
+  grunt.registerTask('default', ['jade','watch', 'less','jasmine', 'connect:server']);
+};
--- less.sh
+++
@@ -1,3 +0,0 @@
-lessc /Applications/MAMP/htdocs/sites/hacktuel-final/MusicBoxMongo/MusicBox/public/css/grid.less > /Applications/MAMP/htdocs/sites/hacktuel-final/MusicBoxMongo/MusicBox/public/css/grid.css
-lessc /Applications/MAMP/htdocs/sites/hacktuel-final/MusicBoxMongo/MusicBox/public/css/charset.less > /Applications/MAMP/htdocs/sites/hacktuel-final/MusicBoxMongo/MusicBox/public/css/charset.css
-
