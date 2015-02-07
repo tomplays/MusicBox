7c8e0716a0b690c1f0d4e8c739863539909414f8
tomplays <homeof@gmail.com>
2015-02-07 23:59:16 +0100

    new files for sub_fragment, user as a service

---
+++ public/js/angular-modules/user_services.js
@@ -0,0 +1,43 @@
+
+angular.module('musicBox.UserService', [])
+.factory("UserService", function($rootScope) {
+
+  
+  var UserService = function() {
+     console.log('UserService')
+     $rootScope.userin = new Object({'username':''});
+  };
+
+
+  UserService.prototype.SetFromApi = function(data) {
+      if(data){
+        $rootScope.userin = data;
+      }
+   };
+
+  UserService.prototype.SetFromLocale = function() {
+     if(USERIN){
+       $rootScope.userin = USERIN;
+     }
+  }
+
+ UserService.prototype.MapOptions = function(data) {
+          $rootScope.documents = [];
+          $rootScope.documents = data.user_documents;
+          var options_array = [];
+          _.each(data.user.user_options , function(option){
+            //console.log(option)
+             // console.log(option)
+              var op_name = option.option_name;
+              var op_value = option.option_value;
+              var op_type = option.option_type;
+              options_array[op_name]= [];
+              options_array[op_name]['value'] = op_value;
+          });
+          $rootScope.user_options = [];
+          $rootScope.user_options = options_array
+  }
+
+  return UserService;
+
+});
