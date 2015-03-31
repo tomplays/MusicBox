a2024ed31119ad4dbf7a865eecb346f7750aef30
tomplays <homeof@gmail.com>
2015-03-31 08:21:06 +0200

    remove useless package

--- api/passport.js
+++ api/passport.js
@@ -1,38 +1,14 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    //TwitterStrategy = require('passport-twitter').Strategy,
-    FacebookStrategy = require('passport-facebook').Strategy,
-    LocalStrategy = require('passport-local').Strategy,
+request = require('request'),
+//TwitterStrategy = require('passport-twitter').Strategy,
+//  FacebookStrategy = require('passport-facebook').Strategy,
+LocalStrategy = require('passport-local').Strategy,
+//BufferAppStrategy = require('passport-bufferapp').Strategy,
 
-    //BufferAppStrategy = require('passport-bufferapp').Strategy,
-    User = mongoose.model('User'),
-    request = require('request');
-
-var nconf = require('nconf');
-//nconf.argv().env().file({file:'config.json'});
-
-var facebook = new Object({
-        'clientID': nconf.get('FACEBOOK_ID'),
-        'clientSecret': nconf.get('FACEBOOK_SECRET'),
-        'callbackURL': nconf.get("ROOT_URL")+"/auth/facebook/callback" // 
-});
-
-
-/*
-var facebook = new Object({
-        'clientID': "717370198340854",
-        'clientSecret': "a66279a9a1995742a66431b6eee82a56",
-        'callbackURL': "http://localhost/auth/facebook/callback" // 
-});
-
-var facebook = new Object({
-        'clientID': "718013574943183",
-        'clientSecret': "6ded2f8da5c3e3840a22e797be148cf9",
-        'callbackURL': "http://2a85b8be5a.url-de-test.ws/auth/facebook/callback" // 
-});
-*/
-
+User = mongoose.model('User'),
+nconf = require('nconf');
 
 module.exports = function(passport) {
     //Serialize sessions
@@ -42,50 +18,44 @@ module.exports = function(passport) {
 
     passport.deserializeUser(function(id, done) {
         //console.log('deserialize')
-      
         User.findOne({
             _id: id
         }, '-salt -hashed_password', function(err, user) {
-         //   console.log('user in')
-
+            //   console.log('user in')
             done(err, user);
         });
         
     });
 
   passport.use(new LocalStrategy(function(username, password, done) {
-User.findOne({ username: username }, function(err, user) {
-if (err) { 
-    console.log('err')
-    return done(err); }
-if (!user) { 
-    console.log('Unknown user')
- return done(null, false, { message: 'Unknown user ' + username }); 
-
-
-}
-
-user.authenticate(password, function(cb) {
-     if(cb){
-        return done(null, user);
-
-     }
-     else{
-         return done(err);
-     }
-})
-
- //console.log(password)
- // done(err, user);
-
-});
+    User.findOne({ username: username }, function(err, user) {
+        if (err) { 
+            console.log('err')
+            return done(err); }
+        if (!user) { 
+            console.log('Unknown user')
+            return done(null, false, { message: 'Unknown user ' + username }); 
+        }
+        user.authenticate(password, function(cb) {
+             if(cb){
+                return done(null, user);
+             }
+             else{
+                 return done(err);
+             }
+        })
+    });
 }));
     
-
-
-
-
+    // keep track / not used anymore
     //Use facebook strategy
+    /*
+    var facebook = new Object({
+        'clientID': nconf.get('FACEBOOK_ID'),
+        'clientSecret': nconf.get('FACEBOOK_SECRET'),
+        'callbackURL': nconf.get("ROOT_URL")+"/auth/facebook/callback" // 
+    });
+
     passport.use(new FacebookStrategy({
             clientID: facebook.clientID,
             clientSecret: facebook.clientSecret,
@@ -126,4 +96,5 @@ user.authenticate(password, function(cb) {
            
         }
     ));
+    */
 };
